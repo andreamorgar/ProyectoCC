03ddb3b1709009fda8df37bcf5ebafdd4e70e313
Andrea Morales <andreamorgarz@gmail.com>
2018-11-15 00:43:10 +0100

    #21 corregir asserts de test sobre clase weather y comprobar funcionamiento

--- test/test_clase.py
+++ test/test_clase.py
@@ -22,10 +22,12 @@ class TestCase(unittest.TestCase):
     def test_object_from_class(self):
 
         # Comprobamos que el objeto prediccion es de la clase Prediction
-        self.assertIsInstance(self.pred, weather_class.Prediction)
+        self.assertIsInstance(self.pred, weather_class.Prediction,
+        "Comprobación de que la instancia es de la clase")
 
         # Como existen elementos de la clase, no puede ser 0
-        self.assertTrue(self.pred.ID>0)
+        self.assertTrue(self.pred.ID>0,
+        "Comprobación de que el objeto se ha creado y el valor de ID ha aumentado")
 
         pass
     # --------------------------------------------------------------------------
@@ -33,26 +35,33 @@ class TestCase(unittest.TestCase):
 
 
         # Comprobamos que el parámetro city devuelve lo que tiene que devolver
-        self.assertEqual(self.pred["city"],self.city)
+        self.assertEqual(self.pred["city"],self.city,
+        "Comprobación de que el atributo city se ha asignado correctamente")
 
         # Comprobamos que el parámetro temperatura devuelve lo correcto
-        self.assertEqual(self.pred["temperature"],self.temp)
+        self.assertEqual(self.pred["temperature"],self.temp,
+        "Comprobación de que el atributo temperature es correcto")
 
         # Como es el primer elemento añadido, su ID tiene que coincidir con
         # el siguiente valor de ID.
-        self.assertEqual(self.pred["ID"],self.previous_id+1)        
-        self.assertEqual(self.pred["ID"],weather_class.Prediction.ID)
+        self.assertEqual(self.pred["ID"],self.previous_id+1,
+        "Comprobación de que el atributo ID es correcto")
+        self.assertEqual(self.pred["ID"],weather_class.Prediction.ID,
+        "Comprobación de que el atributo ID tiene el valor que le corresponde")
         actual_ID = weather_class.Prediction.ID
+
         # Ahora comprobamos que la asignación de ID es correcta
         # Si ahora creamos otro objeto, su id tiene que ser 2.
         self.pred2 = weather_class.Prediction(self.city,self.temp)
-        self.assertEqual(self.pred2["ID"],actual_ID+1)
+        self.assertEqual(self.pred2["ID"],actual_ID+1,
+        "Comprobación de que el valor del ID se incrementa de forma correcta")
 
 
         #Si creamos tres objetos más, debe ser 4
         self.pred3 = weather_class.Prediction(self.city,self.temp)
         self.pred4 = weather_class.Prediction(self.city,self.temp)
-        self.assertEqual(self.pred4["ID"],actual_ID+3)
+        self.assertEqual(self.pred4["ID"],actual_ID+3,
+        "Comprobación de que el valor del ID se incrementa de forma correcta")
 
         pass
 
@@ -63,13 +72,16 @@ class TestCase(unittest.TestCase):
         self.pred.set_city(self.new_city)
 
         # Comprobamos que tras el cambio no es "Madrid",
-        self.assertNotEqual(self.pred["city"],self.city)
+        self.assertNotEqual(self.pred["city"],self.city,
+        "Comprobación de que el valor de city se modifica de forma correcta")
 
         # Comprobamos que tras el cambio, es "Barcelona"
-        self.assertEqual(self.pred["city"],self.new_city)
+        self.assertEqual(self.pred["city"],self.new_city,
+        "Comprobación de que el valor de city se modifica al correcto")
 
         # Me aseguro
-        self.assertEqual(self.pred["city"],"Barcelona")
+        self.assertEqual(self.pred["city"],"Barcelona",
+        "Comprobación de que el valor de city es el que queremos")
         pass
 
     # --------------------------------------------------------------------------
@@ -79,13 +91,16 @@ class TestCase(unittest.TestCase):
         self.pred.set_temperature(self.new_temp)
 
         # Comprobamos que tras el cambio no es "Madrid",
-        self.assertNotEqual(self.pred["temperature"],self.temp)
+        self.assertNotEqual(self.pred["temperature"],self.temp,
+        "Comprobación de que el valor de city se modifica de forma correcta")
 
         # Comprobamos que tras el cambio, es "Barcelona"
-        self.assertEqual(self.pred["temperature"],self.new_temp)
+        self.assertEqual(self.pred["temperature"],self.new_temp,
+        "Comprobación de que el valor de city se modifica al correcto")
 
         # Me aseguro
-        self.assertEqual(self.pred["temperature"],30.0)
+        self.assertEqual(self.pred["temperature"],30.0,
+        "Comprobación de que el valor de city es el que queremos")
 
         # https://stackoverflow.com/questions/129507/how-do-you-test-that-a-python-function-throws-an-exception
         # self.assertRaises("AssertionError", self.pred.set_temperature)
@@ -95,22 +110,28 @@ class TestCase(unittest.TestCase):
     def test_types(self):
         # Compruebo que la temperatura es un número
         self.assertTrue(type(self.pred.temperature) == float or
-        type(self.pred.temperature) == int)
+        type(self.pred.temperature) == int,
+        "Comprobación de que el atributo temperature es del tipo correcto")
 
         # Compruebo que la ciudad es un string
-        self.assertIsInstance(self.pred.city, str)
+        self.assertIsInstance(self.pred.city, str,
+        "Comprobación de que el atributo ciudad es del tipo correcto")
 
         # Compruebo que el tipo ID es un entero
-        self.assertIsInstance(self.pred.ID, int)
-        self.assertTrue(self.pred.ID > 0)
+        self.assertIsInstance(self.pred.ID, int,
+        "Comprobación de que el atributo ID es del tipo correcto")
+        self.assertTrue(self.pred.ID > 0,
+        "Comprobación de que el atributo ID mayor que cero")
 
-        self.assertIsInstance(self.pred.date, str)
+        self.assertIsInstance(self.pred.date, str,
+        "Comprobación de que el atributo fecha es del tipo correcto")
         pass
 
     # --------------------------------------------------------------------------
     def test_len_object(self):
         # El tamaño de la clase tiene que ser 4, porque tiene 4 atributos.
-        self.assertEqual(len(self.pred),4)
+        self.assertEqual(len(self.pred),4,
+        "Comprobación de que hay 4 atributos")
         pass
 
     # --------------------------------------------------------------------------
@@ -119,11 +140,13 @@ class TestCase(unittest.TestCase):
         # Comprobamos que la fecha está en un formato correcto: los separadores
         # en las posiciones correctas, y el resto de valores son números
 
-        self.assertTrue(self.pred.date[2]=='-' and self.pred.date [5]=='-')
+        self.assertFalse(self.pred.date[2]=='-' and self.pred.date [5]=='-',
+        "Comprobación de que el atributo date tiene el formato correcto")
 
         for i,elem in enumerate(self.pred.date):
             if i is not 2 and i is not 5:
-                self.assertTrue(0<=int(elem)<=9)
+                self.assertTrue(0<=int(elem)<=9,,
+                "Comprobación de que el atributo date tiene el formato correcto")
         pass
 
 
